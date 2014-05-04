6f64983222a7e34578ef419577695262fd3f499d
tipsy <davidaase@hotmail.com>
2014-05-04 09:14:36 +0200

    Added resizable console to code editor.

--- AssignmentSystem/public/javascript/a-problem.js
+++ AssignmentSystem/public/javascript/a-problem.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
     var editors = [];
@@ -10,12 +11,15 @@ $(document).ready(function () {
         $("#ace-editor-wrapper").toggleClass("maximized");
         $("#ae-toggle-fullscreen").toggleClass("fa-expand fa-compress");
         $(editors).each(function() { this.resize(); });
+        setupResizeableEditor(editors);
     });
 
     $(".ae-theme-settings").click(function(){
         var theme = ("ace/theme/"+$(this).attr("id"));
         $(editors).each(function() { this.setTheme(theme); });
     });
+
+    setupResizeableEditor(editors);
 
 });
 
@@ -24,4 +28,27 @@ function createEditor(editorID){
     editorID.setTheme("ace/theme/eclipse");
     editorID.getSession().setMode("ace/mode/java");
     return editorID;
+}
+
+function setupResizeableEditor(editors) {
+    $(function () {
+        $(".ace-editor-instance").resizable({
+            handles: 's',
+            maxHeight: $("#ace-editor-wrapper").height()-100,
+            resize: function (e, ui) {
+                var parent = ui.element.parent();
+                var remainingSpace = parent.height() - ui.element.outerHeight(),
+                    divTwo = ui.element.next(),
+                    divTwoHeight = (remainingSpace - (divTwo.outerHeight() - divTwo.height())) / parent.height() * 100 + "%";
+                divTwo.height(divTwoHeight);
+                $(editors).each(function() { this.resize(); });
+            },
+            stop: function (e, ui) {
+                var parent = ui.element.parent();
+                ui.element.css({
+                    height: ui.element.height() / parent.height() * 100 + "%"
+                });
+            }
+        });
+    });
 }
--- AssignmentSystem/public/javascript/star-rating.js
+++ AssignmentSystem/public/javascript/star-rating.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
     $(".assignment-preview a").find(".progress-bar").each(function(){
-        scoreAdjust( getPercentWidth($(this)), $(this), 30+Math.random()*70);
+        scoreAdjust( $(this), getPercentWidth($(this)), 30+Math.random()*70);
     });
 
     var alwaysMax = $(".assignment-preview a").first().find(".progress-bar");
-    scoreAdjust( getPercentWidth(alwaysMax), alwaysMax, 100);
+    scoreAdjust( alwaysMax, getPercentWidth(alwaysMax), 100);
 
 });
 
@@ -13,15 +13,15 @@ function getPercentWidth(e){
     return Math.round( e.width()/e.parent().width() * 100 );
 }
 
-function scoreAdjust (i, progressBar, targetScore) {
+function scoreAdjust (progressBar, currentScore, targetScore) {
     setTimeout(function () {
-        if(i === 50){ progressBar.parent().parent().find(".fa-star.bronze, .glow.bronze").addClass("star-earned") }
-        if(i === 75){ progressBar.parent().parent().find(".fa-star.silver, .glow.silver").addClass("star-earned") }
-        if(i === 100){progressBar.parent().parent().find(".fa-star.gold,   .glow.gold"  ).addClass("star-earned") }
-        progressBar.width(i+"%");
-        i++;
-        if (i <= targetScore) {
-            scoreAdjust(i, progressBar, targetScore);
+        if(currentScore === 50){ progressBar.parent().parent().find(".fa-star.bronze, .glow.bronze").addClass("star-earned") }
+        if(currentScore === 75){ progressBar.parent().parent().find(".fa-star.silver, .glow.silver").addClass("star-earned") }
+        if(currentScore === 100){progressBar.parent().parent().find(".fa-star.gold,   .glow.gold"  ).addClass("star-earned") }
+        progressBar.width(currentScore+"%");
+        currentScore++;
+        if (currentScore <= targetScore) {
+            scoreAdjust(progressBar, currentScore, targetScore);
         }
     }, 20)
 }
