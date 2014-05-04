82761de8a209d2686a1cd947485c088c08973de5
tipsy <davidaase@hotmail.com>
2014-05-04 04:13:00 +0200

    Created array for editors, improved resize.

--- AssignmentSystem/public/javascript/a-problem.js
+++ AssignmentSystem/public/javascript/a-problem.js
@@ -1,38 +1,21 @@
 $(document).ready(function () {
 
+    var editors = [];
     $(".ace-editor-instance").each(function(){
-        initEditor( $(this).attr("id") );
+        editors.push( createEditor( $(this).attr("id") ));
     });
 
     $("#ae-toggle-fullscreen").click(function(){
-        $("#ace-editor-wrapper").hasClass("maximized") ? leaveFullScreen() : goFullScreen();
         $("#ace-editor-wrapper").toggleClass("maximized");
         $("#ae-toggle-fullscreen").toggleClass("fa-expand fa-compress");
+        $(editors).each(function() { this.resize(); });
     });
 
 });
 
-
-function initEditor(editorID){
+function createEditor(editorID){
     var editorID = ace.edit(editorID);
     editorID.setTheme("ace/theme/eclipse");
     editorID.getSession().setMode("ace/mode/java");
-}
-
-function goFullScreen(){
-    var docElement, request;
-    docElement = document.documentElement;
-    request = docElement.requestFullScreen || docElement.webkitRequestFullScreen || docElement.mozRequestFullScreen || docElement.msRequestFullScreen;
-    if(typeof request!="undefined" && request){
-        request.call(docElement);
-    }
-}
-
-function leaveFullScreen(){
-    var docElement, request;
-    docElement = document;
-    request = docElement.cancelFullScreen|| docElement.webkitCancelFullScreen || docElement.mozCancelFullScreen || docElement.msCancelFullScreen || docElement.exitFullscreen;
-    if(typeof request!="undefined" && request){
-        request.call(docElement);
-    }
+    return editorID;
 }
