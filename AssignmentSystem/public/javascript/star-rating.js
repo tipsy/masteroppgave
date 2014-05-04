679001649bd71a659942b413cc8380beaf9b5e0a
tipsy <davidaase@hotmail.com>
2014-05-04 07:14:39 +0200

    Added fancy star-rating javascript.

---
+++ AssignmentSystem/public/javascript/star-rating.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    var score = 100;
+
+    $(".assignment-preview a").find(".progress-bar").each(function(){
+        scoreAdjust( getPercentWidth($(this)), $(this) );
+    });
+
+});
+
+function getPercentWidth(e){
+    var width = e.width();
+    var parentWidth = e.parent().width();
+    var percent = Math.round( 100*width/parentWidth );
+    return percent;
+}
+
+function scoreAdjust (i, progressBar) {
+    setTimeout(function () {
+        if(i === 50){ progressBar.parent().parent().find(".fa-star.bronze").addClass("star-earned") }
+        if(i === 75){ progressBar.parent().parent().find(".fa-star.silver").addClass("star-earned") }
+        if(i === 100){ progressBar.parent().parent().find(".fa-star.gold").addClass("star-earned") }
+        progressBar.width(i+"%");
+        i++;
+        if (i < 101) {
+            scoreAdjust(i, progressBar);
+        }
+    }, 10)
+}
+
