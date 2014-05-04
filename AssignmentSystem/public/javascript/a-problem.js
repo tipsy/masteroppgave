483a8e5849f7021dbd977db933eef03b5b0b3247
tipsy <davidaase@hotmail.com>
2014-05-04 03:44:02 +0200

    Improved full-screen-mode.

--- AssignmentSystem/public/javascript/a-problem.js
+++ AssignmentSystem/public/javascript/a-problem.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
 
+    var editor1;
+
     $(".ace-editor-instance").each(function(){
         initEditor( $(this).attr("id") );
     });
 
     $("#ae-toggle-fullscreen").click(function(){
+        $("#ace-editor-wrapper").hasClass("maximized") ? leaveFullScreen() : goFullScreen();
         $("#ace-editor-wrapper").toggleClass("maximized");
         $("#ae-toggle-fullscreen").toggleClass("fa-expand fa-compress");
     });
@@ -16,4 +19,22 @@ function initEditor(editorID){
     var editorID = ace.edit(editorID);
     editorID.setTheme("ace/theme/eclipse");
     editorID.getSession().setMode("ace/mode/java");
+}
+
+function goFullScreen(){
+    var docElement, request;
+    docElement = document.documentElement;
+    request = docElement.requestFullScreen || docElement.webkitRequestFullScreen || docElement.mozRequestFullScreen || docElement.msRequestFullScreen;
+    if(typeof request!="undefined" && request){
+        request.call(docElement);
+    }
+}
+
+function leaveFullScreen(){
+    var docElement, request;
+    docElement = document;
+    request = docElement.cancelFullScreen|| docElement.webkitCancelFullScreen || docElement.mozCancelFullScreen || docElement.msCancelFullScreen || docElement.exitFullscreen;
+    if(typeof request!="undefined" && request){
+        request.call(docElement);
+    }
 }
