cc6bee6ff0ee6bc603fe7361ac39e3f0f988705c
tipsy <davidaase@hotmail.com>
2014-05-04 07:32:21 +0200

    Improved fancy star-rating script even more!

--- AssignmentSystem/public/javascript/star-rating.js
+++ AssignmentSystem/public/javascript/star-rating.js
@@ -1,29 +1,27 @@
 $(document).ready(function () {
 
-    var score = 100;
-
     $(".assignment-preview a").find(".progress-bar").each(function(){
-        scoreAdjust( getPercentWidth($(this)), $(this) );
+        scoreAdjust( getPercentWidth($(this)), $(this), 30+Math.random()*70);
     });
+
+    var alwaysMax = $(".assignment-preview a").first().find(".progress-bar");
+    scoreAdjust( getPercentWidth(alwaysMax), alwaysMax, 100);
 
 });
 
 function getPercentWidth(e){
-    var width = e.width();
-    var parentWidth = e.parent().width();
-    var percent = Math.round( 100*width/parentWidth );
-    return percent;
+    return Math.round( e.width()/e.parent().width() * 100 );
 }
 
-function scoreAdjust (i, progressBar) {
+function scoreAdjust (i, progressBar, targetScore) {
     setTimeout(function () {
         if(i === 50){ progressBar.parent().parent().find(".fa-star.bronze, .glow.bronze").addClass("star-earned") }
         if(i === 75){ progressBar.parent().parent().find(".fa-star.silver, .glow.silver").addClass("star-earned") }
         if(i === 100){progressBar.parent().parent().find(".fa-star.gold,   .glow.gold"  ).addClass("star-earned") }
         progressBar.width(i+"%");
         i++;
-        if (i < 101) {
-            scoreAdjust(i, progressBar);
+        if (i <= targetScore) {
+            scoreAdjust(i, progressBar, targetScore);
         }
     }, 20)
 }
