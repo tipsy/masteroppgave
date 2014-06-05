36011c6151e0b42ee7417249463ad5b479360464
tipsy <davidaase@hotmail.com>
2014-06-05 16:38:54 +0200

    Added annotation-functionality to editor.

--- AssignmentSystem/public/javascript/a-problem.js
+++ AssignmentSystem/public/javascript/a-problem.js
@@ -19,18 +19,33 @@ $(document).ready(function () {
         $(editors).each(function() { this.setTheme(theme); });
     });
 
-    $(".hidden-when-editor-maximized").collapsible();
+    $(".hidden-when-editor-maximized").collapsible(); //makes ever header in this div collapsible
+
     $(".collapsing-header").click(function(){
         $(this).find("i").toggleClass("fa-angle-down fa-angle-right");
     });
 
+    var annotationList = [];
+    annotationList.push( new Annotation(1, "This is an error", "error") );
+    annotationList.push( new Annotation(2, "This is a warning", "warning") );
+    annotationList.push( new Annotation(3, "This is information", "info") );
+    updateAnnotations(editors[0], annotationList);
 
 });
 
 function createEditor(editorID){
-    var editorID = ace.edit(editorID);
-    editorID.setTheme("ace/theme/eclipse");
-    editorID.getSession().setMode("ace/mode/java");
-    return editorID;
+    var editor = ace.edit(editorID);
+    editor.setTheme("ace/theme/eclipse");
+    editor.getSession().setMode("ace/mode/java");
+    return editor;
 }
 
+function updateAnnotations(editor, annotations){
+    editor.getSession().setAnnotations(annotations);
+}
+
+function Annotation(lineNumber, message, type){
+    this.row = lineNumber;
+    this.text = message;
+    this.type = type; // "error", "warning", "info"
+}
