d34b40b9e841460a6b2972dea3ee811d2357f254
Christian Rasmussen <christian.rasmussen@me.com>
2014-06-14 05:03:22 +0200

    Renamed field to make it more explicit.

--- no.ntnu.assignmentsystem.model/src/no/ntnu/assignmentsystem/services/akka/messages/UpdateSourceCode.java
+++ no.ntnu.assignmentsystem.model/src/no/ntnu/assignmentsystem/services/akka/messages/UpdateSourceCode.java
@@ -5,11 +5,11 @@ import java.io.Serializable;
 public class UpdateSourceCode implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	public final String id;
+	public final String fileId;
 	public final String sourceCode;
 	
-	public UpdateSourceCode(String id, String sourceCode) {
-		this.id = id;
+	public UpdateSourceCode(String fileId, String sourceCode) {
+		this.fileId = fileId;
 		this.sourceCode = sourceCode;
 	}
 }
